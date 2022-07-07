932883088c44d6a2f1c038ea2d919d5a844c111c
Alex Owusu-Ansah <xan.ansah@gmail.com>
2022-07-07 15:17:21 +0100

    Add scaffold for translate:id page

---
+++ app/routes/translate/$translateId.tsx
@@ -0,0 +1,31 @@
+export default function Translate() {
+  return (
+    <div>
+      <div>
+        <div>
+          <button>Back</button>
+          <div>
+            <span>Source Lang</span>
+            <span>Target Lang</span>
+          </div>
+          <button>Back</button>
+        </div>
+        <div>
+          <button>Return to dashboard</button>
+        </div>
+      </div>
+
+      <div>
+        <p></p>
+      </div>
+
+      <div></div>
+
+      <div></div>
+
+      <div>
+        <button>Check</button>
+      </div>
+    </div>
+  );
+}
