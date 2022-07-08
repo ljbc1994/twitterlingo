33f4e465eccd7f3986ed777d7b589ea90d3e2b35
Louie Colgan <ljbc94@gmail.com>
2022-07-08 13:46:29 +0100

    return to dashboard centered

--- app/routes/translate/$translateId.tsx
+++ app/routes/translate/$translateId.tsx
@@ -220,12 +220,14 @@ export default function Translate() {
               className="w-full rounded-full bg-blue-500 py-2 px-4 text-white hover:cursor-pointer hover:bg-blue-700"
               value="Check"
             />
-            <Link
-              className="mx-auto w-full px-4 text-white hover:underline"
-              to="/dashboard"
-            >
-              Return to dashboard
-            </Link>
+            <div className="flex justify-center mt-5">
+              <Link
+                className="mx-auto w-full px-4 text-white hover:underline text-center"
+                to="/dashboard"
+              >
+                Return to dashboard
+              </Link>
+            </div>
           </div>
         </Form>
       </main>
