6fb4b1538f803291081c51d79bb0e6794d538458
Alex Owusu-Ansah <xan.ansah@gmail.com>
2022-07-08 09:21:36 +0100

    Amend merge conflict

--- app/routes/translate/$translateId.tsx
+++ app/routes/translate/$translateId.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import type { BookmarkTranslation } from "~/services/translation.server";
 import { redirect } from "@remix-run/node";
@@ -30,30 +31,126 @@ export default function Translate() {
   const transition = useTransition();
   const actionData = useActionData();
   
+=======
+import { useState } from "react";
+import { Link, useLoaderData } from "@remix-run/react";
+import { json, LoaderFunction } from "@remix-run/server-runtime";
+import { getTranslation } from "~/models/translation.server";
+import { getUser } from "~/services/session.server";
+
+// type LoaderData = {
+//   translation: Awaited<ReturnType<typeof getTranslation>>;
+// };
+
+// export const loader: LoaderFunction = async ({ request }) => {
+//   const user = await getUser(request);
+//   const translation = await getTranslation(user!.id, user!.accessToken);
+
+//   return json<LoaderData>({ translation });
+// };
+
+export default function Translate() {
+  // const data = useLoaderData() as LoaderData;
+  const data = {
+    translation: {
+      bookmarkId: "1544768821873020930",
+      completed: false,
+      id: null,
+      sourceLangCode: "en",
+      sourceLangText:
+        "Votre muscle le plus fort et votre pire ennemi est votre esprit. Entraînez-le bien.",
+      targetLangCode: "fr",
+      targetLangText:
+        "Your strongest muscle and worst enemy is your mind. Train it well.",
+      userId: "id#1540354283811610626",
+    },
+  };
+
+  const { translation } = data;
+  const { sourceLangCode, sourceLangText, targetLangCode, targetLangText } =
+    translation;
+
+  const targetLangTextSplitString: string[] = targetLangText.split(" ");
+
+  const [target, setTarget] = useState<string[]>([]);
+
+>>>>>>> 26e0763... Update translateId page with word activity logic
   return (
     <Form>
       <div>
         <div>
-          <button>Back</button>
+          {/* <Link to={`/translate:${}`}>Prev</Link> */}
           <div>
-            <span>Source Lang</span>
-            <span>Target Lang</span>
+            <span>{sourceLangCode}</span>
+            <span>&gt;</span>
+            <span>{targetLangCode}</span>
           </div>
-          <button>Back</button>
+          {/* <Link to={`/translate:${}`}>Next</Link> */}
         </div>
         <div>
-          <button>Return to dashboard</button>
+          <Link to="/dashboard">Return to dashboard</Link>
         </div>
       </div>
 
       <div>
+<<<<<<< HEAD
         <p>{data.translation?.sourceLangText}</p>
         <p>{data.translation?.targetLangText}</p>
+=======
+        <p>{sourceLangText}</p>
+>>>>>>> 26e0763... Update translateId page with word activity logic
       </div>
 
-      <div></div>
+      <div>
+        {target.map(function (word, idx) {
+          return (
+            <span
+              key={idx}
+              style={{
+                margin: "8px",
+                padding: "12px",
+                backgroundColor: "blue",
+                color: "white",
+                border: "2px solid blue",
+                borderRadius: "4px",
+              }}
+            >
+              {word}
+            </span>
+          );
+        })}
+      </div>
 
-      <div></div>
+      <div>
+        {targetLangTextSplitString.map(function (word, idx) {
+          return (
+            <span
+              key={idx}
+              style={{
+                margin: "8px",
+                padding: "12px",
+                border: "2px solid blue",
+                borderRadius: "4px",
+                ...(target.includes(word) && {
+                  backgroundColor: "grey",
+                  color: "grey",
+                  border: "2px solid grey",
+                }),
+              }}
+              onClick={() =>
+                setTarget(function (prevState) {
+                  if (prevState.includes(word)) {
+                    return prevState;
+                  }
+                  return [...prevState, word];
+                })
+              }
+            >
+              {word}
+            </span>
+          );
+        })}
+      </div>
 
       <div>
         <input type="submit" value="Check" />
