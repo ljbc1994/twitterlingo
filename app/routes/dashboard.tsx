f8ce72e8cf079d6a11b64d1b834cdb4ad850416f
Alex Owusu-Ansah <xan.ansah@gmail.com>
2022-07-08 09:15:17 +0100

    Update scaffold for dashboard ui

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,6 +1,35 @@
+import { useLoaderData } from "@remix-run/react";
+import { json, LoaderFunction } from "@remix-run/server-runtime";
 import { useState } from "react";
+import { languages } from "~/constants/languages";
+import { getUser } from "~/services/session.server";
+import { getTranslationsByUser } from "~/services/translation.server";
+
+type LoaderData = {
+  translations: Awaited<ReturnType<typeof getTranslationsByUser>>;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getUser(request);
+  const translations = await getTranslationsByUser(user!.id, user!.accessToken);
+
+  return json<LoaderData>({ translations });
+};
 
 export default function Dashboard() {
+  const data = useLoaderData() as LoaderData;
+  const { translations } = data;
+
+  const pendingTranslations = translations.filter(function (t) {
+    return !t.completed;
+  });
+
+  const completedTranslations = translations.filter(function (t) {
+    return t.completed;
+  });
+
+  console.log("data", translations);
+
   const [showCompleted, setShowCompleted] = useState<boolean>(false);
 
   return (
@@ -12,6 +41,13 @@ export default function Dashboard() {
         <div>
           <select>
             <option>Please select a language...</option>
+            {languages.map(function ({ name, langCode }) {
+              return (
+                <option key={langCode} value={langCode}>
+                  {name}
+                </option>
+              );
+            })}
           </select>
         </div>
       </div>
@@ -24,11 +60,33 @@ export default function Dashboard() {
         <div>
           {showCompleted ? (
             <div>
-              <p>Here are the completed translations.</p>
+              <p>Here are your completed translations.</p>
+              <ul>
+                {completedTranslations.map(function () {
+                  return <li>t</li>;
+                })}
+              </ul>
             </div>
           ) : (
-            <div>Here are the pending translations.</div>
+            <div>
+              <p>Here are your pending translations.</p>{" "}
+              <ul>
+                {pendingTranslations.map(function ({ sourceLangText }) {
+                  return (
+                    <li>
+                      <p>{sourceLangText}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           )}
+        </div>
+        <div>
+          <p>
+            You don't have any translations. Bookmark a tweet on from Twitter
+            feed to get started.
+          </p>
         </div>
       </div>
     </div>
